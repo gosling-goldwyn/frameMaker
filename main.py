63f45957dd7d64d9952ecbce438d552ce4fb6d29
gosling-goldwyn <m0ulinr0u9e.touch@gmail.com>
2024-08-16 17:44:55 +0900

    [feat]角丸機能追加

--- main.py
+++ main.py
@@ -1,14 +1,19 @@
 import argparse
 import numpy
 import cv2
-from numpy.core.fromnumeric import transpose
+import numpy as np
 from colorpick import getMainColor
+from PIL import Image,ImageDraw
+
+GOLDEN_RATIO = 1.618
+SIDE_MARGIN_RATIO = 0.309 # (1-GOLDEN_RATIO)/2
 
 class ReadError(Exception):
     pass
 
 class FrameMaker():
-    def __init__(self, fp, golden, black):
+    radius = 40
+    def __init__(self, fp, golden, black, rounded, mc):
         img = cv2.imread(fp, cv2.IMREAD_COLOR)
 
         # 読み込みに失敗すると例外
@@ -21,26 +26,46 @@ class FrameMaker():
 
         self.golden = golden
         self.black = black
+        self.rounded = rounded
+        self.mc = mc
         self.transpose = False
         self.isSquare = self.height == self.width
         self.fp = fp
+        self.isWidthBase = False
+        if self.rounded:
+            if not self.black:
+                mask = Image.new("RGB",(self.width,self.height), "White")
+            else:
+                mask = Image.new("RGB",(self.width,self.height), "Black")
+            pil_roundMask = ImageDraw.Draw(mask)
+            if not self.black:
+                pil_roundMask.rounded_rectangle([(1,1),(self.width-1,self.height-1)],self.radius,fill="Black",outline="Black",width=1)
+            else:
+                pil_roundMask.rounded_rectangle([(1,1),(self.width-1,self.height-1)],self.radius,fill="White",outline="White",width=1)
+            self.roundMask = np.array(mask)/255
 
     def run(self):
-
+        if self.rounded:
+            if not self.black:
+                self.img = self.img + self.roundMask
+            else:
+                self.img = self.img * self.roundMask
         # 縦長なら転置して横長にする
         if self.height > self.width:
             self.img = self.img.transpose(1, 0, 2)
             self.height, self.width, self.color = self.img.shape
             self.transpose = True
+        elif int((1.618*self.height-self.width)//2) < 0:
+            self.golden = False
 
         # 新しい画像の辺の長さを計算(黄金比1.618に基づく)
         if self.height < self.width:
             if self.golden:
-                new_width = int(self.height * 1.618)
+                new_width = int(self.height * GOLDEN_RATIO)
             else:
                 new_width = int(self.width)
         else:
-            new_width = int(self.height * 1.618)
+            new_width = int(self.height * GOLDEN_RATIO)
 
         # 白枠をつけるか黒枠をつけるか
         new_img = numpy.zeros([new_width, new_width, self.color], dtype="float64") \
@@ -48,27 +73,27 @@ class FrameMaker():
 
         # 画像を枠の中に配置する起点の計算
         if self.isSquare:
-            sh = int(self.height*0.309)
-            sw = int(self.width*0.309)
+            sh = int(self.height*SIDE_MARGIN_RATIO)
+            sw = int(self.width*SIDE_MARGIN_RATIO)
         elif self.golden:
-            sh = int(self.height*0.309)
-            sw = int((1.618*self.height-self.width)//2)
+            sh = int(self.height*SIDE_MARGIN_RATIO)
+            sw = int((GOLDEN_RATIO*self.height-self.width)//2)
         else:
             sh = (self.width - self.height) // 2
             sw = 0
 
         new_img[sh:sh+self.height, sw:sw+self.width] = self.img
 
-        if self.golden:
+        if self.mc:
             pickWidth = int(self.width // 5)
-            pickHeight = int((self.height*0.309)//5)
+            pickHeight = int((self.height*SIDE_MARGIN_RATIO)//5)
             picker = getMainColor(self.fp,pickWidth,pickHeight)
             new_img[new_width-pickHeight:new_width, sw:sw+pickWidth*5] = picker/255
 
         if self.transpose:
             new_img = new_img.transpose(1, 0, 2)
 
-        return (new_img * 255).astype(numpy.int)
+        return (new_img * 255).astype(int)
 
 
 if __name__ == "__main__":
@@ -80,9 +105,13 @@ if __name__ == "__main__":
                         help='make blank spots the golden ratio in landscape or portrait image')
     PARSER.add_argument("-b", "--black", action='store_true',
                         default=False, help='turn the blank area from white to black')
+    PARSER.add_argument("-r", "--rounded", action='store_true',
+                        default=False, help='turn the rectangle\'s edge from sharp to rounded')
+    PARSER.add_argument("-m", "--maincolor", action='store_true',
+                        default=False, help='put the five main color in the image')
     ARGS = PARSER.parse_args()
     try:
-        FM = FrameMaker(ARGS.input, ARGS.golden, ARGS.black)
+        FM = FrameMaker(ARGS.input, ARGS.golden, ARGS.black, ARGS.rounded, ARGS.maincolor)
         RESULT = FM.run()
     except ReadError:
         print('Read Error: File doesn\'t exist (unsupported japanese characters)')
