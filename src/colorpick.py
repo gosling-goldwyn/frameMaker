c4e4ddd8557d918cdd30eb2bca8b16bbcaec0545
gg mayer <62285092+gosling-goldwyn@users.noreply.github.com>
2025-06-30 11:43:00 +0900

    feat: Complete test code for FrameMaker (#12)
    - Added comprehensive test cases for FrameMaker, including golden ratio, black/white frames, rounded corners, and main color bar functionalities.
    - Refactored existing tests to use appropriate image fixtures for better test coverage.
    - Updated pyproject.toml and uv.lock to include pytest as a development dependency.
    - Adjusted FrameMaker and WebviewInterface to align with testing requirements and improve functionality.

--- src/colorpick.py
+++ src/colorpick.py
@@ -50,3 +50,33 @@ def getMainColorKmeans(img: np.ndarray, width: int, height: int) -> np.ndarray:
         picker[:height, width * i : width * (i + 1)] = color_img
 
     return picker
+
+
+def getMainColorRGBValue(img: np.ndarray) -> list[str]:
+    # 3チャンネルの画像を(縦*横,3)の行列に変換
+    cv2_img = img.reshape((img.shape[0] * img.shape[1], 3))
+
+    # 彩度を計算
+    img_8bit = (img * 255).astype(np.uint8)
+    hsv_image = cv2.cvtColor(img_8bit, cv2.COLOR_BGR2HSV).reshape(
+        (img.shape[0] * img.shape[1], 3)
+    )
+    hsv_image = hsv_image / 255
+    saturation_pixel = hsv_image[:, 1] / hsv_image[:, 1].max()  # 彩度
+    brightness_pixel = hsv_image[:, 2] / hsv_image[:, 2].max()  # 明度
+    saturation_threshold = 0.5
+    brightness_threshold = 0.5
+    saturation_mask = saturation_pixel > saturation_threshold
+    brightness_mask = brightness_pixel > brightness_threshold
+    filtered_pixels = cv2_img[saturation_mask & brightness_mask]
+
+    # KMeansの実行
+    cluster = KMeans(n_clusters=5)
+    cluster.fit(filtered_pixels)
+
+    # cluster.cluster_centers_にRGBがリストとして格納される
+
+    cluster_centers_arr = cluster.cluster_centers_ * 255
+    cluster_centers_arr = cluster_centers_arr.astype("int")
+
+    return [f"{e[0]:02x}{e[1]:02x}{e[2]:02x}" for e in cluster_centers_arr.tolist()]
